0f1b6ec849b660731eda72bedfd41c671e490f9b
Reinhard Sandtner <reinhard.sandtner@pensionsversicherung.at>
2015-02-06 15:17:43 +0100

    added logging

--- was_bug12/was_bug12_jar/src/main/java/at/struct/wasbugs/wasbug12/be/CacheUsingService.java
+++ was_bug12/was_bug12_jar/src/main/java/at/struct/wasbugs/wasbug12/be/CacheUsingService.java
@@ -1,9 +1,14 @@
 package at.struct.wasbugs.wasbug12.be;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @ApplicationScoped
 public class CacheUsingService {
+
+    private static final Logger log = Logger.getLogger(CacheUsingService.class.getName());
+
 
     private volatile SimpleCache cache;
     private volatile int reloads;
@@ -11,13 +16,20 @@ public class CacheUsingService {
 
 
     public CacheInfo getCacheInfo() {
+        log();
         return new CacheInfo(cache.getLastUpdate(), reloads);
     }
 
 
     void setCache(SimpleCache cache) {
+        log();
+
         reloads++;
         this.cache = cache;
     }
 
+
+    private void log() {
+        log.log(Level.INFO, "current thread: {0} - value of reloads: {1}", new Object[] {Thread.currentThread().getName(), reloads});
+    }
 }
